b2bcf890533bbfec3a59a5c3ed2d27cd9a5e8ef0
yandemerova-ru <yandemerovaraisa@gmail.com>
2025-04-30 16:38:08 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -15,76 +15,76 @@ document.addEventListener("DOMContentLoaded", () => {
     // *    5.2. Нет: продолжаем.
     // * 6. Конец.
 
-// Динамический вывод карточек
-// const newsCard = document.querySelector('.news');
-// if (newsCard) {
-//     const newsWrap = newsCard.querySelector('.news__wrap');
-//     const newsCardData = {
+    // Динамический вывод карточек
+    // const newsCard = document.querySelector('.news');
+    // if (newsCard) {
+    //     const newsWrap = newsCard.querySelector('.news__wrap');
+    //     const newsCardData = {
 
-//         news1: {
-//             image: 'images/minnie1.jpg', 
-//             subtitle: 'Новость 1',
-//             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
-//             button: 'Читать дальше',
-//         },
-//         news2: {
-//             image: 'images/jisoo1.jpg', 
-//             subtitle: 'Новость 2',
-//             desc:'Джису из BLACKPINK выпустила свой сольный дебютный мини-альбом «Amortage» вместе с музыкальным видео на заглавную песню «Earthquake»',
-//             button: 'Читать дальше',
-//         },
-//         news3: {
-//             image: 'images/felix1.jpg', 
-//             subtitle: 'Новость 3',
-//             desc:'Участник Stray Kids Феликс сломал руку после «незначительной» автомобильной аварии, о которой сообщил его лейбл JYP Entertainment',
-//             button: 'Читать дальше',
-//         },
-//         news4: {
-//             image: 'images/Cosmosy', 
-//             subtitle: 'Новость 4',
-//             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
-//             button: 'Читать дальше',
-//         },
-//         news5: {
-//             image: 'images/echo.png', 
-//             subtitle: 'Новость 5',
-//             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
-//             button: 'Читать дальше',
-//         },
-//         news6: {
-//             image: 'images/jennie.png', 
-//             subtitle: 'Новость 6',
-//             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
-//             button: 'Читать дальше',
-//         },
-//         news7: {
-//             image: 'images/jin.png', 
-//             subtitle: 'Новость 7',
-//             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
-//             button: 'Читать дальше',
-//         },
+    //         news1: {
+    //             image: 'images/minnie1.jpg', 
+    //             subtitle: 'Новость 1',
+    //             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
+    //             button: 'Читать дальше',
+    //         },
+    //         news2: {
+    //             image: 'images/jisoo1.jpg', 
+    //             subtitle: 'Новость 2',
+    //             desc:'Джису из BLACKPINK выпустила свой сольный дебютный мини-альбом «Amortage» вместе с музыкальным видео на заглавную песню «Earthquake»',
+    //             button: 'Читать дальше',
+    //         },
+    //         news3: {
+    //             image: 'images/felix1.jpg', 
+    //             subtitle: 'Новость 3',
+    //             desc:'Участник Stray Kids Феликс сломал руку после «незначительной» автомобильной аварии, о которой сообщил его лейбл JYP Entertainment',
+    //             button: 'Читать дальше',
+    //         },
+    //         news4: {
+    //             image: 'images/Cosmosy', 
+    //             subtitle: 'Новость 4',
+    //             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
+    //             button: 'Читать дальше',
+    //         },
+    //         news5: {
+    //             image: 'images/echo.png', 
+    //             subtitle: 'Новость 5',
+    //             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
+    //             button: 'Читать дальше',
+    //         },
+    //         news6: {
+    //             image: 'images/jennie.png', 
+    //             subtitle: 'Новость 6',
+    //             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
+    //             button: 'Читать дальше',
+    //         },
+    //         news7: {
+    //             image: 'images/jin.png', 
+    //             subtitle: 'Новость 7',
+    //             desc:'(G)I-DLE вокалистка Минни выпустила свой дебютный сольный сингл «Her» вместе с одноимённым мини-альбомом',
+    //             button: 'Читать дальше',
+    //         },
 
-//     }
+    //     }
 
-//     const createCard = (image, subtitle, desc, button) => {
-//         const card = `
-//         <div class="news__card">
-//             <img class="news__image" src="${image}" alt="Фото" width="200">
-//             <p class="news__subtitle">${subtitle}</p>
-//             <p class="news__desc">${desc}</p>
-//             <button class="news__link" data-id="1"> ${button}</button>
-//         </div>
-//         `;
-//         return card;
-//     }
-//     for (const cardKey in newsCardData) {
-//         const card = newsCardData[cardKey];
-//         const cardElement = createCard(card.image, card.subtitle, card.desc, card.button);
-//         newsCard.insertAdjacentHTML('beforeend', cardElement);
-//     }
-// }
+    //     const createCard = (image, subtitle, desc, button) => {
+    //         const card = `
+    //         <div class="news__card">
+    //             <img class="news__image" src="${image}" alt="Фото" width="200">
+    //             <p class="news__subtitle">${subtitle}</p>
+    //             <p class="news__desc">${desc}</p>
+    //             <button class="news__link" data-id="1"> ${button}</button>
+    //         </div>
+    //         `;
+    //         return card;
+    //     }
+    //     for (const cardKey in newsCardData) {
+    //         const card = newsCardData[cardKey];
+    //         const cardElement = createCard(card.image, card.subtitle, card.desc, card.button);
+    //         newsCard.insertAdjacentHTML('beforeend', cardElement);
+    //     }
+    // }
 
-// Модальное окно
+    // Модальное окно
     const news__link = document.querySelectorAll(".news__link");
     // news__link.addEventListener('click', () => {
     //     console.log ("Нажали на кнопку Читать дальше");
@@ -95,8 +95,8 @@ document.addEventListener("DOMContentLoaded", () => {
             const dataid = item.dataset.id;
             const modalApplication = document.querySelector(".applications[data-id='" + dataid + "']")
             modalApplication.removeAttribute("hidden")
-        
-// Закрытие модального окна
+
+            // Закрытие модального окна
             window.addEventListener("click", (event) => {
                 if (event.target === modalApplication) {
                     modalApplication.setAttribute("hidden", true)
@@ -110,38 +110,38 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-
-// Создание слайдера
-    let currentIndex = 0; 
+/*
+    // Создание слайдера
+    let currentIndex = 0;
     const slider = document.querySelectorAll(".news__card");
     const prevButton = document.querySelector(".news__left");
     const nextButton = document.querySelector(".news__right");
     const visibleCards = 3;
     updateSlider();
 
-    if (prevButton){
-    prevButton.addEventListener("click", () =>{
-        if (currentIndex > 0) {
-            currentIndex--;
-        } else {
-            currentIndex = slider.length - visibleCards;
-        }
-        updateSlider();
-    });
-}
-if (nextButton){
-    nextButton.addEventListener("click", () =>{
-        if (currentIndex < slider.length - visibleCards) {
-            currentIndex++;
-        } else {
-            currentIndex = 0;
-        }
-        updateSlider();
-    });
-}
+    if (prevButton) {
+        prevButton.addEventListener("click", () => {
+            if (currentIndex > 0) {
+                currentIndex--;
+            } else {
+                currentIndex = slider.length - visibleCards;
+            }
+            updateSlider();
+        });
+    }
+    if (nextButton) {
+        nextButton.addEventListener("click", () => {
+            if (currentIndex < slider.length - visibleCards) {
+                currentIndex++;
+            } else {
+                currentIndex = 0;
+            }
+            updateSlider();
+        });
+    }
 
     function updateSlider() {
-        slider.forEach((item, index)=>{
+        slider.forEach((item, index) => {
             if (index >= currentIndex && index < currentIndex + visibleCards) {
                 item.style.display = "flex";
             } else {
@@ -149,56 +149,56 @@ if (nextButton){
             }
         });
     }
+*/
 
+    // Массивы
+    const newsContainer = document.querySelector(".news");
+    if (newsContainer) {
+        const dataTitleNews = [
+            "Минни из (G)I-DLE дебютировала с сольным мини-альбомом «Her»",
+            "Джису выпустила дебютный мини-альбом «Amortage»",
+            "Феликс сломал руку после автомобильной аварии",
+            "Новость 4",
+            "ECHO - новый альбом Джина из BTS",
+            "Новость 6",
+            "Новость 7",
+        ];
+        const titleNews = newsContainer.querySelectorAll(".news__subtitle");
+        titleNews.forEach((item, index) => {
+            item.textContent = dataTitleNews[index];
+        });
+    }
 
-// Массивы
-const newsContainer = document.querySelector (".news");
-if (newsContainer) {
-    const dataTitleNews= [
-        "Минни из (G)I-DLE дебютировала с сольным мини-альбомом «Her»",
-        "Джису выпустила дебютный мини-альбом «Amortage»",
-        "Феликс сломал руку после автомобильной аварии",
-        "Новость 4",
-        "ECHO - новый альбом Джина из BTS",
-        "Новость 6",
-        "Новость 7",
-    ];
-    const titleNews = newsContainer.querySelectorAll(".news__subtitle");
-    titleNews.forEach((item, index) => {
-        item.textContent = dataTitleNews[index];
-    });
-}
-
-//Динамический вывод навигационного меню
-const headerMenu = document.querySelector('.header__menu');
-if (headerMenu){
-    const headerList = headerMenu.querySelector('.header__list');
-    const menuData = {
-        link1: {
-            link:'index.html',
-            title: 'Главная',
-        },
-        link4: {
-            link:'biography.html',
-            title: 'Биография',
-        },
-        link3: {
-            link:'shop.html',
-            title: 'Магазин',
+    //Динамический вывод навигационного меню
+    const headerMenu = document.querySelector('.header__menu');
+    if (headerMenu) {
+        const headerList = headerMenu.querySelector('.header__list');
+        const menuData = {
+            link1: {
+                link: 'index.html',
+                title: 'Главная',
+            },
+            link4: {
+                link: 'biography.html',
+                title: 'Биография',
+            },
+            link3: {
+                link: 'shop.html',
+                title: 'Магазин',
+            }
+        }
+        const createLink = (UrlLink, title) => {
+            const link = `<li class="header__item"><a class="header__link" href="${UrlLink}">${title}</a></li>`;
+            return link;
+        }
+        for (const linkItem in menuData) {
+            const link = menuData[linkItem];
+            const linkIndex = createLink(link.link, link.title);
+            headerList.insertAdjacentHTML('beforeend', linkIndex);
         }
     }
-    const createLink = (UrlLink, title) =>{
-        const link = `<li class="header__item"><a class="header__link" href="${UrlLink}">${title}</a></li>`;
-        return link;
-    }
-    for (const linkItem in menuData) {
-        const link = menuData[linkItem];
-        const linkIndex = createLink(link.link, link.title);
-        headerList.insertAdjacentHTML('beforeend', linkIndex);
-    }
-}
 
-const cardsImages = document.querySelector(".images");
+    const cardsImages = document.querySelector(".images");
     if (cardsImages) {
         const cardListImages = cardsImages.querySelector(".images__list");
 
@@ -232,52 +232,74 @@ const cardsImages = document.querySelector(".images");
                     );
                     cardListImages.insertAdjacentHTML("beforeend", cardElement);
                 });
-             
 
-const pictures = document.querySelectorAll(".images__picture");
-if (pictures) {
-    // Перебираем каждое изображение
-    pictures.forEach((picture) => {
-        picture.addEventListener("click", () => {
-            // Получаем родительский элемент (li)
-            const parentItem = picture.parentElement;
 
-            // Получаем все изображения в родительском элементе
-            const parentPictures =
-                parentItem.querySelectorAll(".images__picture");
+                const pictures = document.querySelectorAll(".images__picture");
+                if (pictures) {
+                    // Перебираем каждое изображение
+                    pictures.forEach((picture) => {
+                        picture.addEventListener("click", () => {
+                            // Получаем родительский элемент (li)
+                            const parentItem = picture.parentElement;
 
-            // Переключаем видимость изображений
-            parentPictures.forEach((parentPictures) => {
-                if (parentPictures !== picture) {
-                    parentPictures.style.display = "block"; // Показываем другое изображение
-                } else {
-                    parentPictures.style.display = "none"; // Скрываем текущее изображение
+                            // Получаем все изображения в родительском элементе
+                            const parentPictures =
+                                parentItem.querySelectorAll(".images__picture");
+
+                            // Переключаем видимость изображений
+                            parentPictures.forEach((parentPictures) => {
+                                if (parentPictures !== picture) {
+                                    parentPictures.style.display = "block"; // Показываем другое изображение
+                                } else {
+                                    parentPictures.style.display = "none"; // Скрываем текущее изображение
+                                }
+                            });
+                        });
+                    });
                 }
+
             });
-        });
-    });
-}
-
-});
     }
-               
 
-// предзагрузчик
-const preloader = document.querySelector(".preloader");
-const content = document.querySelector(".content");
-if (preloader && content) {
-    setTimeout(() => {
-        // Скрываем прелоадер
-        preloader.style.opacity = "0";
-        preloader.style.visibility = "hidden";
 
-        // Показываем контент
-        content.style.display = "block";
+    // предзагрузчик
+    const preloader = document.querySelector(".preloader");
+    const content = document.querySelector(".content");
+    if (preloader && content) {
+        setTimeout(() => {
+            // Скрываем прелоадер
+            preloader.style.opacity = "0";
+            preloader.style.visibility = "hidden";
 
-        // Удаляем элемент из DOM
-        preloader.remove();
-    }, 500); // Задержка 3 секунды
-}
+            // Показываем контент
+            content.style.display = "block";
+
+            // Удаляем элемент из DOM
+            preloader.remove();
+        }, 500); // Задержка 3 секунды
+    }
+
+
+    // объявляем переменную sliders,куда помещаем элемент с классом swiper
+    const sliders = document.querySelector('.swiper');
+    //проверяем существует ли элемент
+    if (sliders) {
+        const swiper1 = new Swiper(sliders, {
+            slidesPerView: 4,
+            spaceBetween: 10,
+            // Пагинация
+            pagination: {
+                el: '.swiper-pagination',
+                type: "fraction",
+            },
+
+            // Навигационные стрелки
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
 
 
 });
